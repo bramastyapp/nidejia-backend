0f8890fad868a9bec156bfb52472c0e2d3e5c0ee
bramastyapp <bramastyapp@gmail.com>
2024-09-19 05:23:10 +0700

    fix: Transaction Resource done

--- app/Filament/Resources/TransactionResource.php
+++ app/Filament/Resources/TransactionResource.php
@@ -7,8 +7,12 @@ use App\Filament\Resources\TransactionResource\RelationManagers;
 use App\Models\Transaction;
 use Filament\Forms;
 use Filament\Forms\Form;
+use Filament\Notifications\Notification;
 use Filament\Resources\Resource;
+use Filament\Support\Enums\FontWeight;
 use Filament\Tables;
+use Filament\Tables\Actions\Action;
+use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -27,22 +31,61 @@ class TransactionResource extends Resource
     public static function form(Form $form): Form
     {
         return $form
-            ->schema([
-                //
-            ]);
+            ->schema([]);
     }
 
     public static function table(Table $table): Table
     {
         return $table
             ->columns([
-                //
+                Tables\Columns\TextColumn::make('user.name')
+                    ->numeric()
+                    ->sortable()
+                    ->weight(FontWeight::Bold),
+                Tables\Columns\TextColumn::make('listing.title')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('start_date')
+                    ->date()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('end_date')
+                    ->date()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('total_days')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('total_price')
+                    ->money('USD')
+                    ->sortable()
+                    ->weight(FontWeight::Bold),
+                Tables\Columns\TextColumn::make('status')
+                    ->searchable()
+                    ->badge()
+                    ->color(fn(string $state): string => match ($state) {
+                        'waiting' => 'gray',
+                        'approved' => 'info',
+                        'canceled' => 'danger',
+                    }),
             ])
             ->filters([
-                //
+                SelectFilter::make('status')
+                    ->options([
+                        'waiting' => 'Waiting',
+                        'approved' => 'Approved',
+                        'canceled' => 'Canceled',
+                    ])
+
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                Action::make('approve')
+                    ->button()
+                    ->color('success')
+                    ->requiresConfirmation()
+                    ->action(function (Transaction $transaction) {
+                        Transaction::find($transaction->id)->update(['status' => 'approved']);
+                        Notification::make()->success()->title('Transaction Approved!')->body('Transaction has been approved successfully')->icon('heroicon-o-check')->send();
+                    })
+                    ->hidden(fn(Transaction $transaction) => $transaction->status != 'waiting')
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
